c80438de572e9cf9f13a710ba535c132c347b451
CodingCat <zhunansjtu@gmail.com>
2014-03-14 22:52:47 -0400

    start rate control location

--- simulation/src/main/scala/network/forwarding/controlplane/openflow/RivuaiControlPlane.scala
+++ simulation/src/main/scala/network/forwarding/controlplane/openflow/RivuaiControlPlane.scala
@@ -1,8 +1,11 @@
 package scalasem.network.forwarding.controlplane.openflow
 
+import scalasem.network.events.RivuaiRateControlEvent
 import scalasem.network.forwarding.controlplane.openflow.flowtable.{OFRivuaiFlowTableEntry, OFFlowTable}
+import scalasem.network.forwarding.dataplane.RivuaiDataPlane
 import scalasem.network.topology.{ToRRouterType, HostType, Router}
 import scalasem.network.traffic.Flow
+import scalasem.simengine.SimulationEngine
 import scalasem.util.XmlParser
 
 class RivuaiControlPlane(router: Router) extends OpenFlowControlPlane(router) {
@@ -23,4 +26,20 @@ class RivuaiControlPlane(router: Router) extends OpenFlowControlPlane(router) {
       Double.MaxValue
     }
   }
+
+  private def startRateController() {
+    if (router.nodeType == ToRRouterType) {
+      val startTime = SimulationEngine.startTime
+      val endTime = SimulationEngine.endTime
+      val frequency = XmlParser.getDouble("simengine.rivuai.frequency", 0.005)
+
+      for (s <- startTime until endTime by frequency) {
+        val rcEvent = new RivuaiRateControlEvent(
+          node.dataplane.asInstanceOf[RivuaiDataPlane], s)
+        SimulationEngine.addEvent(rcEvent)
+      }
+    }
+  }
+
+  startRateController()
 }
