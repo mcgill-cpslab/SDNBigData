ffa13523048504809da50e552e926d6adfe28516
CodingCat <zhunansjtu@gmail.com>
2014-03-16 16:34:42 -0400

    fix setting SwitchMap

---
+++ controller/src/main/java/net/floodlightcontroller/networkmonitor/FatTreeRateController.java
@@ -0,0 +1,5 @@
+package net.floodlightcontroller.networkmonitor;
+
+public class FatTreeRateController extends RateController {
+
+}
--- controller/src/main/java/net/floodlightcontroller/networkmonitor/RateController.java
+++ controller/src/main/java/net/floodlightcontroller/networkmonitor/RateController.java
@@ -190,9 +190,6 @@ public class RateController implements IOFMessageListener, IFloodlightModule {
         OFSwitchRateLimitingState slsmsg = (OFSwitchRateLimitingState) msg;
         SwitchRateLimiterStatus obj = new SwitchRateLimiterStatus();
         obj.tablesize = slsmsg.getTablesize();
-        String swip = ((InetSocketAddress)
-                sw.getChannel().getRemoteAddress()).getAddress().getHostAddress();
-        switchMap.put(getIPRange(swip), sw);
         switchRateLimitMap.put(sw, obj);
         //install the flows
         for (FlowInstallRequest request: flowtoInstallList.get(sw)) {
@@ -206,8 +203,12 @@ public class RateController implements IOFMessageListener, IFloodlightModule {
         }
         break;
       case GET_CONFIG_REPLY:
-        if (!flowtoInstallList.containsKey(sw))
+        if (!flowtoInstallList.containsKey(sw)) {
+          String swip = ((InetSocketAddress)
+                  sw.getChannel().getRemoteAddress()).getAddress().getHostAddress();
           flowtoInstallList.put(sw, new ArrayList<FlowInstallRequest>());
+          switchMap.put(getIPRange(swip), sw);
+        }
         break;
     }
     return null;
